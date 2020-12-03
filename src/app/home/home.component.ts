91479eb108cd215add270a2a8845e0e1e546d652
mdemski <Gdemono171>
2020-12-03 09:41:48 +0100

    Rebuild home page.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import {DatabaseService} from '../services/database.service';
+import {Company} from '../models/company';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +10,29 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class HomeComponent implements OnInit {
   // TODO przekazać tutaj id z zalogowanego użytkownika
+  // TODO POŁĄCZYĆ SASSA Z BEMEM SKRÓCENIE PISANIA KODU - PATRZ KURS
   id: any;
+  mostRecentProducts: any = [];
+  availableSellers: any = [];
+  // TODO przygotować koszyk przetrzymujący dodawane z całej aplikacji produkty
+  @Input() cart: any;
 
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService,
+              public db: DatabaseService) {
     translate.addLangs(['pl', 'en', 'fr', 'de']);
     translate.setDefaultLang('pl');
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.mostRecentProducts = this.db.getMostRecentProductsHomePage();
+    this.availableSellers = this.db.getAllSellers();
+  }
 
+  addToCart(product: any) {
+    this.cart.add(product);
+  }
+
+  getAvailableSellers() {
+
+  }
 }
