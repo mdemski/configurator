babcbe01e2ae295a68f276499881f9100cd19e02
mdemski <Gdemono171>
2020-12-29 17:50:42 +0100

    Links to header comment

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-shop',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
+  //TODO dodać linki na stronę w headerze
+  urls = ['strona główna'];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.urls.push(this.router.url.split('/', 2)[1]);
+    console.log(this.urls);
   }
 
 }
