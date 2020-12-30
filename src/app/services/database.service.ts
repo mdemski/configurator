1fa08e595ce1df122003c1130331e7a08e23297f
mdemski <Gdemono171>
2020-12-30 15:45:41 +0100

    Refill missing data and rebuild methods returning single products by id

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -19,47 +19,47 @@ export class DatabaseService {
     return this.windows = [
       new RoofWindowSkylight(1, 'ISO I22 78x118', 'ISO', 'I22', 78, 118, 'Okna dachowe', 'Okna obrotowe',
         'IS', 'obrotowe', 'NEO-AIR', 'drewno', 'bezbarwny', 'przeźroczysty', 'aluminium', 'RAL7022',
-        'gładki', 'Extra Secure', 'RAL7048', false, null, null, 'assets/img/products/ISO-I22.png', 1267),
+        'gładki', 'Extra Secure', 'RAL7048', false, null, null, 'assets/img/products/ISO-I22.png', 1267, 1.06, 0.7),
       new RoofWindowSkylight(2, 'IGOV N22 78x118', 'IGOV', 'N22', 78, 118, 'Okna dachowe', 'Okna obrotowe',
         'IG', 'obrotowe', 'NEO-AIR', 'PVC', 'biały', 'gładki', 'aluminium', 'RAL7022',
-        'gładki', 'Extra Secure', 'RAL9003', false, null, null, 'assets/img/products/IGOV-N22.png', 1460),
+        'gładki', 'Extra Secure', 'RAL9003', false, null, null, 'assets/img/products/IGOV-N22.png', 1460, 0.83, 0.5),
       new RoofWindowSkylight(3, 'ISO E2 78x118', 'ISO', 'E2', 78, 118, 'Okna dachowe', 'Okna obrotowe',
         'IS', 'obrotowe', 'NEO-AIR', 'drewno', 'bezbarwny', 'przeźroczysty', 'aluminium', 'RAL7022',
-        'gładki', 'Extra Secure', 'RAL7048', false, null, null, 'https://www.okpol.pl/wp-content/uploads/2014/10/okno-obrotowe-drewniane.png', 977),
+        'gładki', 'Extra Secure', 'RAL7048', false, null, null, 'https://www.okpol.pl/wp-content/uploads/2014/10/okno-obrotowe-drewniane.png', 977, 1.2, 1.0),
       new RoofWindowSkylight(4, 'IGOV E2 78x118', 'IGOV', 'E2', 78, 118, 'Okna dachowe', 'Okna obrotowe',
         'IG', 'obrotowe', 'NEO-AIR', 'PVC', 'biały', 'gładki', 'aluminium', 'RAL7022',
-        'gładki', 'Extra Secure', 'RAL9003', false, null, null, 'assets/img/products/IGOV-E2.png', 1230),
+        'gładki', 'Extra Secure', 'RAL9003', false, null, null, 'assets/img/products/IGOV-E2.png', 1230, 1.2, 1.0),
       new RoofWindowSkylight(5, 'ISO I22 78x118', 'ISO', 'I22', 78, 118, 'Okna dachowe', 'Okna obrotowe',
         'IS', 'obrotowe', 'NEO-AIR', 'drewno', 'bezbarwny', 'przeźroczysty', 'aluminium', 'RAL7022',
-        'gładki', 'Extra Secure', 'RAL7048', false, null, null, 'assets/img/products/ISO-I22.png', 1267),
+        'gładki', 'Extra Secure', 'RAL7048', false, null, null, 'assets/img/products/ISO-I22.png', 1267, 1.06, 0.7),
       new RoofWindowSkylight(6, 'IGOV N22 78x118', 'IGOV', 'N22', 78, 118, 'Okna dachowe', 'Okna obrotowe',
         'IG', 'obrotowe', 'NEO-AIR', 'PVC', 'biały', 'gładki', 'aluminium', 'RAL7022',
-        'gładki', 'Extra Secure', 'RAL9003', false, null, null, 'assets/img/products/IGOV-N22.png', 1460),
+        'gładki', 'Extra Secure', 'RAL9003', false, null, null, 'assets/img/products/IGOV-N22.png', 1460, 0.83, 0.5),
       new RoofWindowSkylight(7, 'ISO E2 78x118', 'ISO', 'E2', 78, 118, 'Okna dachowe', 'Okna obrotowe',
         'IS', 'obrotowe', 'NEO-AIR', 'drewno', 'bezbarwny', 'przeźroczysty', 'aluminium', 'RAL7022',
-        'gładki', 'Extra Secure', 'RAL7048', false, null, null, 'https://www.okpol.pl/wp-content/uploads/2014/10/okno-obrotowe-drewniane.png', 977),
+        'gładki', 'Extra Secure', 'RAL7048', false, null, null, 'https://www.okpol.pl/wp-content/uploads/2014/10/okno-obrotowe-drewniane.png', 977, 1.2, 1.0),
       new RoofWindowSkylight(8, 'IGOV E2 78x118', 'IGOV', 'E2', 78, 118, 'Okna dachowe', 'Okna obrotowe',
         'IG', 'obrotowe', 'NEO-AIR', 'PVC', 'biały', 'gładki', 'aluminium', 'RAL7022',
-        'gładki', 'Extra Secure', 'RAL9003', false, null, null, 'assets/img/products/IGOV-E2.png', 1230)
+        'gładki', 'Extra Secure', 'RAL9003', false, null, null, 'assets/img/products/IGOV-E2.png', 1230, 1.2, 1.0)
     ];
   }
 
   // TODO do oprogramowania pobieranie danych z eNova/pliku
   getAllAccessoriesToShopList() {
     return this.accessories = [
+      new Accessory(1, 'Decomatic D33Z 78x118', 'D33', 78, 118, 'Akcesoria', 'Rolety wewnętrzne', 'A', 'zaciemniający', 'S001', 'srebrny', null, 'manulana', null, 217),
+      new Accessory(2, 'Decomatic D33Z 78x118', 'D33', 78, 118, 'Akcesoria', 'Rolety wewnętrzne', 'A', 'zaciemniający', 'S001', 'srebrny', null, 'manulana', null, 217),
+      new Accessory(3, 'Decomatic D33Z 78x118', 'D33', 78, 118, 'Akcesoria', 'Rolety wewnętrzne', 'A', 'zaciemniający', 'S001', 'srebrny', null, 'manulana', null, 217),
+      new Accessory(4, 'Decomatic D33Z 78x118', 'D33', 78, 118, 'Akcesoria', 'Rolety wewnętrzne', 'A', 'zaciemniający', 'S001', 'srebrny', null, 'manulana', null, 217),
+      new Accessory(5, 'Decomatic D33Z 78x118', 'D33', 78, 118, 'Akcesoria', 'Rolety wewnętrzne', 'A', 'zaciemniający', 'S001', 'srebrny', null, 'manulana', null, 217),
+      new Accessory(6, 'Decomatic D33Z 78x118', 'D33', 78, 118, 'Akcesoria', 'Rolety wewnętrzne', 'A', 'zaciemniający', 'S001', 'srebrny', null, 'manulana', null, 217),
+      new Accessory(7, 'Decomatic D33Z 78x118', 'D33', 78, 118, 'Akcesoria', 'Rolety wewnętrzne', 'A', 'zaciemniający', 'S001', 'srebrny', null, 'manulana', null, 217),
+      new Accessory(8, 'Decomatic D33Z 78x118', 'D33', 78, 118, 'Akcesoria', 'Rolety wewnętrzne', 'A', 'zaciemniający', 'S001', 'srebrny', null, 'manulana', null, 217),
       new Accessory(9, 'Decomatic D33Z 78x118', 'D33', 78, 118, 'Akcesoria', 'Rolety wewnętrzne', 'A', 'zaciemniający', 'S001', 'srebrny', null, 'manulana', null, 217),
       new Accessory(10, 'Decomatic D33T 78x118', 'D33', 78, 118, 'Akcesoria', 'Rolety wewnętrzne', 'A', 'transparentny', 'A368', 'srebrny', null, 'manulana', null, 157),
       new Accessory(11, 'Multistop D37Z 78x118', 'D37', 78, 118, 'Akcesoria', 'Rolety wewnętrzne', 'A', 'zaciemniający', 'S001', 'srebrny', null, 'manulana', null, 375),
       new Accessory(12, 'Multistop D37T 78x118', 'D37', 78, 118, 'Akcesoria', 'Rolety wewnętrzne', 'A', 'transparentny', 'A368', 'srebrny', null, 'manulana', null, 312)
     ];
-  }
-
-  getWindowById(id: number) {
-    return this.windows[--id];
-  }
-
-  getAccessoryById(id: number) {
-    return this.accessories[id];
   }
 
   fetchRoofWindows(): Observable<any> {
@@ -70,6 +70,14 @@ export class DatabaseService {
   fetchAccessories(): Observable<any> {
     const accessories = this.getAllAccessoriesToShopList();
     return of(accessories);
+  }
+
+  getWindowById(id: number) {
+    return this.getAllRoofWindowsToShopList()[--id];
+  }
+
+  getAccessoryById(id: number) {
+    return this.getAllAccessoriesToShopList()[--id];
   }
 
   // Musi zwracać listę 3 produktów z których 1 jest oknem, 2 to kołnierz, 3 akcesorium
