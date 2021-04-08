d1859fd4eccc45d0bcf32f233480c2afa908c279
mdemski <Gdemono171>
2021-04-08 07:32:40 +0200

    Firebase configuration added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBYX38DKMbLQyk8sRaLARGheE_r8ojrevE',
+    authDomain: 'window-configurator.firebaseapp.com',
+    databaseURL: 'https://window-configurator.firebaseio.com',
+    projectId: 'window-configurator',
+    storageBucket: 'window-configurator.appspot.com',
+    messagingSenderId: '515578583439',
+    appId: '1:515578583439:web:e1b292b753cbc5752a93fa'
+  }
 };
 
 /*
