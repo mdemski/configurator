10b500f8a21343ca869c1e9689741eef9d1bb917
mdemski <Gdemono171>
2021-04-28 20:57:05 +0200

    Files deleted

--- src/app/models/configuration.model.ts
+++
@@ -1,9 +0,0 @@
-import {RoofWindowSkylight} from './roof-window-skylight';
-import {Flashing} from './flashing';
-import {Accessory} from './accessory';
-
-export interface ConfigurationModel {
-  window: RoofWindowSkylight | null;
-  flashing: Flashing | null;
-  accessory: Accessory | null;
-}
