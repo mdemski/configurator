86a1066de1182859c3b93628cdbf6f30bdc70db0
mdemski <Gdemono171>
2021-05-26 21:12:00 +0200

    Added new method for firestore and userId

--- src/app/services/highest-id-getter.service.ts
+++ src/app/services/highest-id-getter.service.ts
@@ -1,13 +1,32 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {SingleConfiguration} from '../models/single-configuration';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {BehaviorSubject} from 'rxjs';
+import {AuthService} from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HighestIdGetterService {
   highestId: number;
+  configurationGlobalId$ = new BehaviorSubject(1);
+  configurationUserId$ = new BehaviorSubject(1);
 
-  constructor() {
+  constructor(private firestore: AngularFirestore,
+              private auth: AuthService) {
     this.highestId = 0;
+    this.auth.returnUser().subscribe(user => {
+      this.firestore.collection('allConfigurations').valueChanges().pipe().subscribe((allConfigurations: SingleConfiguration[]) => {
+        let userIdNumber = 1;
+        this.configurationGlobalId$.next(allConfigurations.length + 1);
+        allConfigurations.forEach(config => {
+          if (config.user === user) {
+            userIdNumber++;
+          }
+        });
+        this.configurationUserId$.next(userIdNumber);
+      });
+    });
   }
 
   getHighestId(initialId, configurations: any[]) {
@@ -21,5 +40,19 @@ export class HighestIdGetterService {
     }
     this.highestId++;
     return this.highestId;
+  }
+
+  getHighestIdFormFireStore() {
+    let configurationId = 1;
+    this.configurationGlobalId$.subscribe(id => configurationId = id);
+    this.configurationGlobalId$.next(configurationId + 1);
+    return configurationId;
+  }
+
+  getHighestIdForUser() {
+    let userId = 1;
+    this.configurationUserId$.subscribe(id => userId = id);
+    this.configurationUserId$.next(userId + 1);
+    return userId;
   }
 }
