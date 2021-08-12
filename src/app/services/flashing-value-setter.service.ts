e5051c1a05010d483731417c18ac116bbb1752ca
mdemski <Gdemono171>
2021-08-12 11:06:34 +0200

    Flashings array of model creator (for kombi - with lShaped) added

--- src/app/services/flashing-value-setter.service.ts
+++ src/app/services/flashing-value-setter.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import {Injectable, OnDestroy} from '@angular/core';
 import {Flashing} from '../models/flashing';
 import {TranslateService} from '@ngx-translate/core';
 import {takeUntil} from 'rxjs/operators';
@@ -6,7 +6,7 @@ import {takeUntil} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
-export class FlashingValueSetterService {
+export class FlashingValueSetterService implements OnDestroy {
 
   isDestroyed$;
 
@@ -15,7 +15,12 @@ export class FlashingValueSetterService {
     translate.setDefaultLang('pl');
   }
 
-  setModelName(flashing: Flashing) {
+  ngOnDestroy(): void {
+    this.isDestroyed$.next();
+  }
+
+
+  setModelNameFromErpData(flashing: Flashing) {
     // tslint:disable-next-line:max-line-length
     flashing.flashingName = this.buildFlashingModel(flashing.model, flashing.szerokosc, flashing.wysokosc);
   }
@@ -28,6 +33,100 @@ export class FlashingValueSetterService {
     });
     const flashingType = model.split(':')[1];
     return String(flashingDesc + ' ' + flashingType + ' ' + szerokosc + 'x' + wysokosc);
+  }
+
+  setTileHeight(flashing: Flashing) {
+    const apronType = flashing.typFartucha;
+    switch (apronType) {
+      case ('U'):
+        flashing.flashingTileHeight = 5;
+        break;
+      case ('H'):
+        flashing.flashingTileHeight = 9;
+        break;
+      case ('H5'):
+        flashing.flashingTileHeight = 5;
+        break;
+      case ('H9'):
+        flashing.flashingTileHeight = 9;
+        break;
+      case ('H12'):
+        flashing.flashingTileHeight = 12;
+        break;
+      case ('K'):
+        flashing.flashingTileHeight = 5;
+        break;
+      case ('A'):
+        flashing.flashingTileHeight = 5;
+        break;
+      case ('BRAK'):
+        flashing.flashingTileHeight = 0;
+        break;
+      default:
+        flashing.flashingTileHeight = 5;
+        break;
+    }
+  }
+
+  // Flashing models in combinations setter - return list of flashing models (Kołnierz:KXP H9, Kołnierz:KXL H9, Kołnierz:KYS H12, ...)
+  flashingsArrayOfModelsCreator(verticalNumber: number, horizontalNumber: number, lShaped: string, flashingType: string): string[] {
+    const flashingsModelArray: string[] = [];
+    const lShapedConnection = lShaped === 'Kołnierz:KK';
+    if (flashingType === 'Kołnierz:RESET') {
+      const numberOfFlashings = verticalNumber * horizontalNumber;
+      for (let i = 0; i < numberOfFlashings; i++) {
+        flashingsModelArray.push(flashingType);
+      }
+    } else {
+      // FlashingType - BP, U, R, P, L, LH, D
+      const splitFlashingType = flashingType.split(':')[1];
+      // Kombinacja z kołnierzami wyłącznie w pionie wraz z kołnierzami kolankowymi
+      if (verticalNumber === 1) {
+        if (lShapedConnection) {
+          flashingsModelArray.push(String('Kołnierz:KK1 ' + splitFlashingType));
+        } else {
+          flashingsModelArray.push(flashingType);
+        }
+        const flashingModel = String('Kołnierz:KYS ' + splitFlashingType);
+        for (let i = 1; i < horizontalNumber; i++) {
+          flashingsModelArray.push(flashingModel);
+        }
+      }
+      // Kombinacja z kołnierzami w pinie i pozimie wraz z kołnierzami kolankowymi
+      // Normalne dolne
+      const bottomLeftFlashing = String('Kołnierz:KXP ' + splitFlashingType);
+      const bottomRightFlashing = String('Kołnierz:KXL ' + splitFlashingType);
+      const bottomMiddleFlashing = String('Kołnierz:KXS ' + splitFlashingType);
+      // Kolankowe dolne
+      const bottomLeftLShaped = String('Kołnierz:KKP ' + splitFlashingType);
+      const bottomRightLShaped = String('Kołnierz:KKL ' + splitFlashingType);
+      const bottomMiddleLShaped = String('Kołnierz:KKS ' + splitFlashingType);
+      // Normalne górne
+      const topLeftFlashing = String('Kołnierz:KZP ' + splitFlashingType);
+      const topRightFlashing = String('Kołnierz:KZL ' + splitFlashingType);
+      const topMiddleFlashing = String('Kołnierz:KZS ' + splitFlashingType);
+      if (lShapedConnection) {
+        flashingsModelArray.push(bottomLeftLShaped);
+        for (let i = 2; i < verticalNumber; i++) {
+          flashingsModelArray.push(bottomMiddleLShaped);
+        }
+        flashingsModelArray.push(bottomRightLShaped);
+      } else {
+        flashingsModelArray.push(bottomLeftFlashing);
+        for (let i = 2; i < verticalNumber; i++) {
+          flashingsModelArray.push(bottomMiddleFlashing);
+        }
+        flashingsModelArray.push(bottomRightFlashing);
+      }
+      for (let i = 1; i < horizontalNumber; i++) {
+        flashingsModelArray.push(topLeftFlashing);
+        for (let j = 2; i < verticalNumber; j++) {
+          flashingsModelArray.push(topMiddleFlashing);
+        }
+        flashingsModelArray.push(topRightFlashing);
+      }
+    }
+    return flashingsModelArray;
   }
 
   // setNumberOfConnections(flashing: Flashing) {
@@ -87,37 +186,4 @@ export class FlashingValueSetterService {
   //       break;
   //   }
   // }
-
-  setTileHeight(flashing: Flashing) {
-    const apronType = flashing.typFartucha;
-    switch (apronType) {
-      case ('U'):
-        flashing.flashingTileHeight = 5;
-        break;
-      case ('H'):
-        flashing.flashingTileHeight = 9;
-        break;
-      case ('H5'):
-        flashing.flashingTileHeight = 5;
-        break;
-      case ('H9'):
-        flashing.flashingTileHeight = 9;
-        break;
-      case ('H12'):
-        flashing.flashingTileHeight = 12;
-        break;
-      case ('K'):
-        flashing.flashingTileHeight = 5;
-        break;
-      case ('A'):
-        flashing.flashingTileHeight = 5;
-        break;
-      case ('BRAK'):
-        flashing.flashingTileHeight = 0;
-        break;
-      default:
-        flashing.flashingTileHeight = 5;
-        break;
-    }
-  }
 }
