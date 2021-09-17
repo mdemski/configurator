678c2ddbedd73fc81456a3fa7c8c5b8c811abac9
mdemski <Gdemono171>
2021-09-17 08:36:05 +0200

    Custom router implementation added

---
+++ src/app/store/router/router.state.ts
@@ -0,0 +1,43 @@
+import {Params} from '@angular/router';
+import {Selector, State} from '@ngxs/store';
+import {RouterStateModel as RouterStateOuterModel} from '@ngxs/router-plugin/src/router.state';
+
+export interface RouterStateModel {
+  url: string;
+  params: Params;
+  queryParams: Params;
+  data: any;
+}
+
+@State<RouterStateModel>({
+  name: 'customRouter',
+  defaults: {
+    url: '',
+    params: null,
+    queryParams: null,
+    data: null
+  }
+})
+
+export class RouterState {
+
+  @Selector([RouterState])
+  static data({state}: RouterStateOuterModel<RouterStateModel>) {
+    return state.data;
+  }
+
+  @Selector([RouterState])
+  static params({state}: RouterStateOuterModel<RouterStateModel>) {
+    return state.params;
+  }
+
+  @Selector([RouterState])
+  static queryParams({state}: RouterStateOuterModel<RouterStateModel>) {
+    return state.queryParams;
+  }
+
+  @Selector([RouterState])
+  static url({state}: RouterStateOuterModel<RouterStateModel>) {
+    return state.url;
+  }
+}
