f772a2f4b4e0dd7db54a7ba192a7fbd62f26f4d1
mdemski <Gdemono171>
2021-10-02 09:26:54 +0200

    Filtration for roof windows refactored - working!

--- src/app/shop/roof-windows/roof-windows.component.ts
+++ src/app/shop/roof-windows/roof-windows.component.ts
@@ -1,7 +1,6 @@
-import {Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {RoofWindowSkylight} from '../../models/roof-window-skylight';
-import {Router} from '@angular/router';
-import {shareReplay, takeUntil} from 'rxjs/operators';
+import {takeUntil} from 'rxjs/operators';
 import {Select} from '@ngxs/store';
 import {RoofWindowState} from '../../store/roof-window/roof-window.state';
 import {Observable, Subject} from 'rxjs';
@@ -12,7 +11,16 @@ import {Observable, Subject} from 'rxjs';
   styleUrls: ['./roof-windows.component.scss']
 })
 export class RoofWindowsComponent implements OnInit, OnDestroy {
-  filters: object;
+  filters: {
+    windowName: string,
+    windowGlazings: string[],
+    windowOpeningTypes: string[],
+    windowMaterials: string[],
+    windowWidthFrom: number,
+    windowWidthTo: number,
+    windowHeightFrom: number,
+    windowHeightTo: number
+  };
   @Input() searchByKeyboard: string;
   @Select(RoofWindowState.roofWindows) roofWindows$: Observable<RoofWindowSkylight[]>;
   roofWindowsList: RoofWindowSkylight[] = [];
@@ -23,81 +31,93 @@ export class RoofWindowsComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.roofWindows$.pipe(takeUntil(this.isDestroyed$), shareReplay()).subscribe(roofWindows => this.roofWindowsList = roofWindows);
-    // this.loadAllWindows().filter = JSON.stringify(this.filteredValues);
-    this.filterWindowList(JSON.stringify(this.filters), this.roofWindowsList);
+    this.roofWindows$.pipe(takeUntil(this.isDestroyed$)).subscribe(roofWindows => this.roofWindowsList = roofWindows);
+    this.filteredRoofWindowsList = this.roofWindowsList;
   }
 
   filtersInput(filtersObject: any) {
     this.filters = filtersObject;
-    console.log(filtersObject);
+    let numberOfGlazingNull = 0;
+    let filteredByGlazing: RoofWindowSkylight[];
+    let numberOfOpeningNull = 0;
+    let filteredByOpening: RoofWindowSkylight[];
+    let numberOfMaterialNull = 0;
+    let filteredByMaterial: RoofWindowSkylight[];
+    this.filters.windowGlazings.forEach(glazing => {
+      if (glazing === null) {
+        numberOfGlazingNull++;
+      }
+    });
+    this.filters.windowOpeningTypes.forEach(opening => {
+      if (opening === null) {
+        numberOfOpeningNull++;
+      }
+    });
+    this.filters.windowMaterials.forEach(material => {
+      if (material === null) {
+        numberOfMaterialNull++;
+      }
+    });
+    if (numberOfGlazingNull === this.filters.windowGlazings.length) {
+      filteredByGlazing = this.roofWindowsList;
+    } else {
+      filteredByGlazing = this.roofWindowsList
+        .filter(window => this.filters.windowGlazings.includes(window.glazingToCalculation));
+    }
+    if (numberOfOpeningNull === this.filters.windowOpeningTypes.length) {
+      filteredByOpening = filteredByGlazing;
+    } else {
+      filteredByOpening = filteredByGlazing
+        .filter(window => this.filters.windowOpeningTypes.includes(window.otwieranie));
+    }
+    if (numberOfMaterialNull === this.filters.windowMaterials.length) {
+      filteredByMaterial = filteredByOpening;
+    } else {
+      filteredByMaterial = filteredByOpening
+        .filter(window => this.filters.windowMaterials.includes(window.stolarkaMaterial));
+    }
+    this.filteredRoofWindowsList = filteredByMaterial.filter(window => {
+      let nameFound = true;
+      let widthFound = true;
+      let heightFound = true;
+      if (this.filters.windowName) {
+        nameFound = window.windowName.toString().trim().toLowerCase().indexOf(this.filters.windowName.toLowerCase()) !== -1;
+      }
+      if (this.filters.windowWidthFrom !== undefined && this.filters.windowWidthTo !== undefined) {
+        // widths
+        if (this.filters.windowWidthFrom <= this.filters.windowWidthTo) {
+          widthFound = window.szerokosc >= this.filters.windowWidthFrom && window.szerokosc <= this.filters.windowWidthTo;
+        }
+        if (this.filters.windowWidthTo < 20 && this.filters.windowWidthFrom > 10) {
+          widthFound = window.szerokosc >= this.filters.windowWidthFrom;
+        }
+        if (this.filters.windowWidthFrom < 10 && this.filters.windowWidthTo > 20) {
+          widthFound = window.szerokosc <= this.filters.windowWidthTo;
+        }
+        if (this.filters.windowWidthFrom < 10 && this.filters.windowWidthTo < 20) {
+          widthFound = true;
+        }
+      }
+      if (this.filters.windowHeightFrom !== undefined && this.filters.windowHeightTo !== undefined) {
+        // heights
+        if (this.filters.windowHeightFrom <= this.filters.windowHeightTo) {
+          heightFound = window.wysokosc >= this.filters.windowHeightFrom && window.wysokosc <= this.filters.windowHeightTo;
+        }
+        if (this.filters.windowHeightTo < 20 && this.filters.windowHeightFrom > 10) {
+          heightFound = window.wysokosc >= this.filters.windowHeightFrom;
+        }
+        if (this.filters.windowHeightFrom < 10 && this.filters.windowHeightTo > 20) {
+          heightFound = window.wysokosc <= this.filters.windowHeightTo;
+        }
+        if (this.filters.windowHeightFrom < 10 && this.filters.windowHeightTo < 20) {
+          heightFound = true;
+        }
+      }
+      return nameFound && widthFound && heightFound;
+    });
   }
 
   ngOnDestroy() {
     this.isDestroyed$.next();
-  }
-
-
-  // loadAllWindows() {
-  //   this.filteredRoofWindowsList = this.filteredRoofWindowsList.length > 0 ? this.filteredRoofWindowsList : this.roofWindowsList;
-  // }
-
-  filterWindowList(filters: any, roofWindowsList: RoofWindowSkylight[]) {
-    this.filteredRoofWindowsList = roofWindowsList;
-    if (filters) {
-      const keys = Object.keys(filters);
-
-      // TODO Dodać tutaj ngFor sprawdzający dla wszystkich wartości a nie pojedynczej wartości klucza patrz co pokazuje result poniżej
-
-      const filterWindow = window => {
-        let result = keys.map(key => {
-          if ((!~key.indexOf('windowWidth')) && (!~key.indexOf('windowHeight'))) {
-            if (window[key]) {
-              return String(window[key]).toLowerCase().startsWith(String(filters[key]).toLowerCase());
-            } else {
-              return false;
-            }
-          }
-          // if (!~key.indexOf('windowHeight')) {
-          //   if (window[key]) {
-          //     return String(window[key]).toLowerCase().startsWith(String(filters[key]).toLowerCase());
-          //   } else {
-          //     return false;
-          //   }
-          // }
-        });
-// To Clean Array from undefined if the age is passed so the map will fill the gap with (undefined)
-        result = result.filter(it => it !== undefined);
-// Filter the from the other filters
-        // Width window/skylight filtration
-        if (filters.windowWidthTo >= 0 && filters.windowWidthFrom >= 0) {
-          if (window.windowWidth) {
-            if (+window.windowWidth >= +filters.windowWidthFrom && +window.windowWidth <= +filters.windowWidthTo) {
-              result.push(true);
-            } else {
-              result.push(false);
-            }
-          } else {
-            result.push(false);
-          }
-        }
-        // Height window/skylight filtration
-        if (filters.windowHeightTo >= 0 && filters.windowHeightFrom >= 0) {
-          if (window.windowHeight) {
-            if (+window.windowHeight >= +filters.windowHeightFrom && +window.windowHeight <= +filters.windowHeightTo) {
-              result.push(true);
-            } else {
-              result.push(false);
-            }
-          } else {
-            result.push(false);
-          }
-        }
-        return result.reduce((acc, cur: any) => {
-          return acc && cur;
-        }, 1);
-      };
-      this.filteredRoofWindowsList = this.roofWindowsList.filter(filterWindow);
-    }
   }
 }
