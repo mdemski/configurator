01cfa7116d60cd1e9fbc31f814a82e3b24609951
mdemski <Gdemono171>
2021-10-21 18:13:38 +0200

    Address class rebuilt.

--- src/app/models/address.ts
+++ src/app/models/address.ts
@@ -1,21 +1,49 @@
-interface Localization {
-  coordinateA: number;
-  coordinateB: number;
-}
-
 export class Address {
+  private _firstName: string;
+  private _lastName: string;
+  private _phoneNumber: string;
   private _street: string;
   private _address: string;
   private _zipCode: string;
   private _country: string;
-  private _localization: Localization;
+  private _localization: {
+    coordinateA: number;
+    coordinateB: number;
+  };
 
-  constructor(street: string, address: string, zipCode: string, country: string, localization: Localization) {
+  constructor(firstName: string, lastName: string, phoneNumber: string, street: string, address: string, zipCode: string, country: string, localization: { coordinateA: number; coordinateB: number }) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._phoneNumber = phoneNumber;
     this._street = street;
     this._address = address;
     this._zipCode = zipCode;
     this._country = country;
     this._localization = localization;
+  }
+
+  get firstName(): string {
+    return this._firstName;
+  }
+
+  set firstName(value: string) {
+    this._firstName = value;
+  }
+
+  get lastName(): string {
+    return this._lastName;
+  }
+
+  set lastName(value: string) {
+    this._lastName = value;
+  }
+
+  get phoneNumber(): string {
+    return this._phoneNumber;
+  }
+
+  set phoneNumber(value: string) {
+    this._phoneNumber = value;
   }
 
   get street(): string {
@@ -50,11 +78,11 @@ export class Address {
     this._country = value;
   }
 
-  get localization(): Localization {
+  get localization(): { coordinateA: number; coordinateB: number } {
     return this._localization;
   }
 
-  set localization(value: Localization) {
+  set localization(value: { coordinateA: number; coordinateB: number }) {
     this._localization = value;
   }
 }
