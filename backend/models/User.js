b695cd2ba0b723b0451a04d5cb3799be8f8fcf55
mdemski <Gdemono171>
2021-10-23 16:23:33 +0200

    Updated user class

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,12 +3,6 @@ const Schema = mongoose.Schema;
 
 // Define collection and schema
 let User = new Schema({
-  firstName: {
-    type: String
-  },
-  lastName: {
-    type: String
-  },
   email: {
     type: String
   },
