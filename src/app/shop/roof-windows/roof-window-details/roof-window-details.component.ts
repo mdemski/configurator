e2bfe0ea7c9ae842117ff5e801e9643e481b1029
mdemski <Gdemono171>
2021-11-05 08:14:52 +0100

    Discount prices fixed.

--- src/app/shop/roof-windows/roof-window-details/roof-window-details.component.ts
+++ src/app/shop/roof-windows/roof-window-details/roof-window-details.component.ts
@@ -8,6 +8,7 @@ import {Observable, Subject} from 'rxjs';
 import {RouterState} from '@ngxs/router-plugin';
 import {RoofWindowState} from '../../../store/roof-window/roof-window.state';
 import {AppState} from '../../../store/app/app.state';
+import {CrudService} from '../../../services/crud-service';
 
 @Component({
   selector: 'app-roof-window-details',
@@ -18,7 +19,8 @@ export class RoofWindowDetailsComponent implements OnInit, OnDestroy {
   window$: Observable<RoofWindowSkylight>;
   @Select(AppState) user$: Observable<{ currentUser }>;
   logInUser: {
-    user: User;
+    email: string;
+    user: string;
     loggedIn: boolean
   };
   routerData;
@@ -28,13 +30,13 @@ export class RoofWindowDetailsComponent implements OnInit, OnDestroy {
   windowMaterial: string;
   windowVent: string;
   windowHandle: string;
-  priceAfterDisc: any;
+  priceAfterDisc$ = new Subject<number>();
   glazing: string;
   availableSizes = ['55x78', '55x98', '66x98', '66x118', '66x140', '78x98', '78x118', '78x140', '78x160', '94x118', '94x140', '94x160', '114x118', '114x140', '134x98'];
   quantity = 1;
   availableExtras: Accessory[] = [];
 
-  constructor(private store: Store) {
+  constructor(private store: Store, private crud: CrudService) {
     this.user$.pipe(takeUntil(this.isDestroyed$)).subscribe(user => this.logInUser = user.currentUser);
     this.store.select(RouterState.state).pipe(takeUntil(this.isDestroyed$)).subscribe(state => {
       this.window$ = this.store.select(RoofWindowState.roofWindowByCode)
@@ -56,7 +58,7 @@ export class RoofWindowDetailsComponent implements OnInit, OnDestroy {
     this.picturesOfWindow.push('assets/img/products/ISO-I22.png');
     this.picturesOfWindow.push('assets/img/products/ISO-arrangement-1.png');
     this.picturesOfWindow.push('assets/img/products/ISO-arrangement-2.png');
-    this.priceAfterDisc = this.getDiscountPrice();
+    this.getDiscountPrice();
     // TODO napisać obsługę tej metody z wykorzystaniem store'a
     // this.availableExtras.push(this.db.getAccessoryById(1), this.db.getAccessoryById(2));
   }
@@ -67,7 +69,9 @@ export class RoofWindowDetailsComponent implements OnInit, OnDestroy {
 
   getDiscountPrice() {
     if (this.logInUser.loggedIn) {
-      return (this.windowToShow.CenaDetaliczna - (this.windowToShow.CenaDetaliczna * this.logInUser.user.discount));
+      return this.crud.readUserByEmail(this.logInUser.email).pipe(takeUntil(this.isDestroyed$)).subscribe(user => {
+        this.priceAfterDisc$.next(this.windowToShow.CenaDetaliczna / (1 + user.discount));
+      });
     }
   }
 
