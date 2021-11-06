eba9b75a41a6dec6503161c208b3eee51d02736f
mdemski <Gdemono171>
2021-11-06 10:13:23 +0100

    Add comment to do.

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -9,6 +9,7 @@ export class AuthInterceptor implements HttpInterceptor {
   // Klasa która dodaje do każdego zapytania HTTP JWT
   // tslint:disable-next-line:max-line-length
   // TODO do zweryfikowania czy nie potrzebujemy logiki która nie będzie doklejała JWT w odniesieniu do np. konkretnej roli, dodatkowo do ustalenia czy któreś ścieżki w expressie nie potrzebują autoryzacji JWT?
+  // TODO w przypadku gdy zapytanie musi mieć JWT token, a go nie posiada express serwer powinien zwrócić błąd
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const loginUser: LoginUser = JSON.parse(localStorage.getItem('loginUser'));
     if (loginUser && loginUser.token) {
