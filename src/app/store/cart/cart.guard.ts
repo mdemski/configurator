82dfc9702c0831e0449de96ec29a84d0d8c6c4d1
mdemski <Gdemono171>
2021-12-09 07:22:06 +0100

    Deleted unused code.

--- src/app/store/cart/cart.guard.ts
+++ src/app/store/cart/cart.guard.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {CanActivate} from '@angular/router';
 import {Store} from '@ngxs/store';
-import {Observable} from 'rxjs';
 import {CartState} from './cart.state';
 import {GetCart} from './cart.actions';
 
@@ -10,7 +9,7 @@ export class CartGuard implements CanActivate {
   constructor(private store: Store) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate() {
     if (!this.store.selectSnapshot(CartState.cart)) {
       this.store.dispatch(new GetCart());
       return true;
