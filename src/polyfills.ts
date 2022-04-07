f2b42c70bd30fb62626e3678788c98cdd02308b1
mardem <m.demski@outlook.com>
2022-04-07 18:35:45 +0200

    Update to version 13

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,16 +18,6 @@
  * BROWSER POLYFILLS
  */
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/**
- * Web Animations `@angular/platform-browser/animations`
- * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
- * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
- */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
@@ -61,12 +51,9 @@ import 'zone.js'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-(window as any).global = window;
-(window as any).global.Buffer = require('buffer').Buffer;
-// // (window as any).process = {};
 (window as any).process = {
   env: {
     NODE_DEBUG: undefined,
-    NODE_ENV: undefined
+    // NODE_ENV: undefined
   },
 };
