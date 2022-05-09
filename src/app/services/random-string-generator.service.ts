ad2305fc73d96bd77bded17d18a9a7b42adc0578
MarDem <m.demski@outlook.com>
2022-05-09 18:34:29 +0200

    Specific chars deleted.

--- src/app/services/random-string-generator.service.ts
+++ src/app/services/random-string-generator.service.ts
@@ -7,7 +7,7 @@ export class RandomStringGeneratorService {
   constructor() { }
 
   randomString(length: number) {
-    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!*()&%^#@';
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!*()';
     let result = '';
     for (let i = length; i > 0; --i) { result += chars[Math.floor(Math.random() * chars.length)]; }
     return result;
