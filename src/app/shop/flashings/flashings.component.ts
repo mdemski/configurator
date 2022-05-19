1e2c9d9444d6950e4355221a55a3557aa48fdc9a
MarDem <m.demski@outlook.com>
2022-05-19 15:08:56 +0200

    Git test

--- src/app/shop/flashings/flashings.component.ts
+++ src/app/shop/flashings/flashings.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Select, Store} from '@ngxs/store';
 import {FlashingState} from '../../store/flashing/flashing.state';
-import {Observable} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 import {Flashing} from '../../models/flashing';
 import {GetFlashings} from '../../store/flashing/flashing.actions';
+import {CartState} from '../../store/cart/cart.state';
 
 @Component({
   selector: 'app-flashings',
@@ -11,16 +12,32 @@ import {GetFlashings} from '../../store/flashing/flashing.actions';
   styleUrls: ['./flashings.component.css']
 })
 export class FlashingsComponent implements OnInit {
-
   @Select(FlashingState.flashings) flashings$: Observable<Flashing[]>;
-  isFiltering: any;
+  @Select(CartState) cart$: Observable<any>;
+  private isDestroyed$ = new Subject();
+  isFiltering = false;
+  filters: {
+    flashingName: string,
+    flashingType: string[],
+    flashingApronType: string[],
+    flashingCombination: string[],
+    flashingMaterialType: string[],
+    flashingMaterialColor: string[],
+    flashingWidthFrom: number,
+    flashingWidthTo: number,
+    flashingHeightFrom: number,
+    flashingHeightTo: number
+  };
+  flashingsList: Flashing[] = [];
+  filteredFlashingsList: Flashing[] = [];
+  searchText: string;
+  page = 1;
+  pageSize = 10;
+  sortBy = 'popularity';
   constructor(private store: Store) { }
 
   ngOnInit(): void {
     this.isFiltering = true;
-    setTimeout(() => {
-      this.isFiltering = false;
-    }, 2000);
     this.store.dispatch(new GetFlashings());
   }
 
