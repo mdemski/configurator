dc5e06f21cc631cc076a1607a1b2754f24940078
MarDem <m.demski@outlook.com>
2022-05-23 18:04:10 +0200

    Rename css styling file

--- src/app/shop/flashings/flashing-details/flashing-details.component.ts
+++ src/app/shop/flashings/flashing-details/flashing-details.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-flashing-details',
   templateUrl: './flashing-details.component.html',
-  styleUrls: ['./flashing-details.component.css']
+  styleUrls: ['./flashing-details.component.scss']
 })
 export class FlashingDetailsComponent implements OnInit {
 
