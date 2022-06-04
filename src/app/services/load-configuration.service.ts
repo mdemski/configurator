3559421e8b02b068800871bae5763c5a20f5bd9d
MarDem <m.demski@outlook.com>
2022-06-04 15:31:28 +0200

    Flat roof window load method added.

--- src/app/services/load-configuration.service.ts
+++ src/app/services/load-configuration.service.ts
@@ -10,6 +10,8 @@ import {RoofWindowState} from '../store/roof-window/roof-window.state';
 import {Store} from '@ngxs/store';
 import {FlashingState} from '../store/flashing/flashing.state';
 import {AccessoryState} from '../store/accessory/accessory.state';
+import {FlatRoofWindow} from '../models/flat-roof-window';
+import {FlatRoofWindowState} from '../store/flat-roof-window/flat-roof-window.state';
 
 @Injectable({
   providedIn: 'root'
@@ -17,20 +19,23 @@ import {AccessoryState} from '../store/accessory/accessory.state';
 export class LoadConfigurationService {
 
   newWindow = new RoofWindowSkylight(
-    null, null, null, null, null, null, null, null, 'dwuszybowy', 78,
+    null, null, null, 'I-OKNO', 'NPL-OKNO', null, null, null, 'dwuszybowy', 78,
     118, 'OknoDachowe', null, null, null, null, 'NawiewnikNeoVent', null, null, null, null,
     null, null, null, null, false, 0, [], [], [],
     [], 0, 0, 0, 0, null, null, null, 0, 'PL');
 
   newFlashing = new Flashing(null, null, null, 'I-KOŁNIERZ', 'NPL-KOŁNIERZ', '1.Nowy', null,
-      78, 118, 'KołnierzUszczelniający', null, null, null, null, null, null, null,
-      null, 0, null, 0, 0, 0, 0,
-      [], [], null, false, null, []);
+    78, 118, 'KołnierzUszczelniający', null, null, null, null, null, null, null,
+    null, 0, null, 0, 0, 0, 0,
+    [], [], null, false, null, []);
 
   // TODO Sprawdzić jaka jest grupa asortymentowa dla rolet wewnętrznych
   newAccessory = new Accessory(null, null, null, 'I-ROLETAW', 'NPL-ROLETAW', '1. Nowy', null, 78, 118, 'Akcesorium',
     null, null, null, null, null, null, null, null, null, null, null, null, null, 0,
     'manualne', null, 0, [], [], null, '');
+
+  newFlatRoofWindow = new FlatRoofWindow(null, null, null, 'I-PŁASKI', 'NPL-OKNO', null, null, null, 'A01', 100, 100, 'DachPłaski', 'DachPłaski:Okno', 'PG', null, null, 'PVC',
+    null, null, null, null, null, null, null, 0, [], [], [], [], 0, null, null, 0, null, null, null, null);
 
   constructor(private crud: CrudService,
               private db: DatabaseService,
@@ -90,6 +95,26 @@ export class LoadConfigurationService {
         return this.store.select(AccessoryState.accessoryByCode).pipe(
           map(filterFn => filterFn(accessoryCode)));
       }
+    }
+  }
+
+  getFlarRoofWindowToReconfiguration(user: string, formName: string, flatRoofWindowCode: string): Observable<FlatRoofWindow> {
+    if (user !== '' || user !== undefined) {
+      if (formName === 'no-name' && flatRoofWindowCode === undefined) {
+        return of(this.newFlatRoofWindow);
+      }
+      if (formName === 'no-name' && flatRoofWindowCode === '-1') {
+        return of(this.newFlatRoofWindow);
+      }
+      if (formName === undefined && flatRoofWindowCode === undefined) {
+        return of(this.newFlatRoofWindow);
+      }
+      if (formName === 'no-name' && flatRoofWindowCode !== undefined) {
+        return this.store.select(FlatRoofWindowState.flatByCode)
+          .pipe(map(filterFn => filterFn(flatRoofWindowCode)));
+      }
+      // tslint:disable-next-line:max-line-length
+      // TODO dodać opcję gdy wklejony zostanie link z konfiguracji zalogowanego użytkownika, a w trakcie wklejania użytkownik jest wylogowany
     }
   }
 }
