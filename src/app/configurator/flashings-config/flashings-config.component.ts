962b56b99af8a842f798706890289f2a60a54bd1
MarDem <m.demski@outlook.com>
2022-09-08 20:02:22 +0200

    Empty config value catches added.

--- src/app/configurator/flashings-config/flashings-config.component.ts
+++ src/app/configurator/flashings-config/flashings-config.component.ts
@@ -679,6 +679,7 @@ export class FlashingsConfigComponent implements OnInit, OnDestroy, AfterViewIni
 
   private setProductPath(verticalNumber, horizontalNumber) {
     this.path = String(verticalNumber + 'x' + horizontalNumber + '.jpg');
+    console.log(this.path);
   }
 
   private setExteriorPath(configuredFlashing: Flashing, verticalNumber, horizontalNumber) {
@@ -750,6 +751,7 @@ export class FlashingsConfigComponent implements OnInit, OnDestroy, AfterViewIni
     } else {
       this.exteriorPath = 'H9-A7022P-1x1.jpg';
     }
+    console.log(this.exteriorPath);
   }
 
   closeAllHovers(htmlDivElements: HTMLDivElement[]) {
