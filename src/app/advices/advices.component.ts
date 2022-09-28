67655bec33099156612669fe8dfbcc7e19120584
MarDem <m.demski@outlook.com>
2022-09-28 21:42:14 +0200

    Advices with filtration configuration

--- src/app/advices/advices.component.ts
+++ src/app/advices/advices.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {Advice} from '../models/advice';
+import advices from '../../assets/json/advices.json';
+import moment from 'moment';
+import {MdTranslateService} from '../services/md-translate.service';
 
 @Component({
   selector: 'app-advices',
@@ -8,27 +12,43 @@ import {Router} from '@angular/router';
 })
 export class AdvicesComponent implements OnInit, OnDestroy {
 
-  private isDestroyed;
+  private isDestroyed$;
   isLoading = true;
   searchAdvice: string;
-  advices: any[];
-  filteredAdvices: any[];
+  advices: Advice[];
+  filteredAdvices: Advice[];
+  currentDate = moment(new Date());
 
-  constructor(public router: Router) {
-    this.searchAdvice = '';
+  constructor(public router: Router, private translate: MdTranslateService) {
+    this.advices = [];
+    for (const advice of advices) {
+      if (this.translate.getBrowserLang() === advice.language) {
+        this.advices.push(new Advice(advice.id, advice.title, advice.type, advice.content, advice.category, advice.picture, advice.link,
+          new Date(advice.added), advice.short, advice.recipients, Boolean(advice.active), advice.language));
+      }
+    }
+    this.filteredAdvices = this.advices;
   }
 
   ngOnInit(): void {
-    this.advices = [];
-    this.filteredAdvices = this.advices;
     this.isLoading = false;
   }
 
   ngOnDestroy(): void {
-    this.isDestroyed.next();
+    this.isDestroyed$.next();
   }
 
   filtering() {
-    console.log(this.searchAdvice);
+    this.filteredAdvices = this.advices;
+    this.filteredAdvices = this.filteredAdvices.filter(advice => {
+      return advice.short.toString().trim().toLocaleLowerCase().indexOf(this.searchAdvice.toLocaleLowerCase()) !== -1 ||
+        advice.content.toString().trim().toLocaleLowerCase().indexOf(this.searchAdvice.toLocaleLowerCase()) !== -1 ||
+        advice.title.toString().trim().toLocaleLowerCase().indexOf(this.searchAdvice.toLocaleLowerCase()) !== -1 ||
+        advice.category.toString().trim().toLocaleLowerCase().indexOf(this.searchAdvice.toLocaleLowerCase()) !== -1;
+    });
+  }
+
+  durationCalc(advice) {
+    return moment.duration(this.currentDate.diff(moment(advice.added))).days();
   }
 }
---
+++ src/app/models/advice.ts
@@ -0,0 +1,126 @@
+export class Advice {
+  private _id: string;
+  private _title: string;
+  private _type: string;
+  private _content: string;
+  private _category: string;
+  private _picture: string;
+  private _link: string;
+  private _added: Date;
+  private _short: string;
+  private _recipients: string;
+  private _active: boolean;
+  private _language: string;
+
+  constructor(id: string, title: string, type: string, content: string, category: string, picture: string, link: string, added: Date,
+              short: string, recipients: string, active: boolean, language: string) {
+    this._id = id;
+    this._title = title;
+    this._type = type;
+    this._content = content;
+    this._category = category;
+    this._picture = picture;
+    this._link = link;
+    this._added = added;
+    this._short = short;
+    this._recipients = recipients;
+    this._active = active;
+    this._language = language;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  set id(value: string) {
+    this._id = value;
+  }
+
+  get title(): string {
+    return this._title;
+  }
+
+  set title(value: string) {
+    this._title = value;
+  }
+
+  get type(): string {
+    return this._type;
+  }
+
+  set type(value: string) {
+    this._type = value;
+  }
+
+  get content(): string {
+    return this._content;
+  }
+
+  set content(value: string) {
+    this._content = value;
+  }
+
+  get category(): string {
+    return this._category;
+  }
+
+  set category(value: string) {
+    this._category = value;
+  }
+
+  get picture(): string {
+    return this._picture;
+  }
+
+  set picture(value: string) {
+    this._picture = value;
+  }
+
+  get link(): string {
+    return this._link;
+  }
+
+  set link(value: string) {
+    this._link = value;
+  }
+
+  get added(): Date {
+    return this._added;
+  }
+
+  set added(value: Date) {
+    this._added = value;
+  }
+
+  get short(): string {
+    return this._short;
+  }
+
+  set short(value: string) {
+    this._short = value;
+  }
+
+  get recipients(): string {
+    return this._recipients;
+  }
+
+  set recipients(value: string) {
+    this._recipients = value;
+  }
+
+  get active(): boolean {
+    return this._active;
+  }
+
+  set active(value: boolean) {
+    this._active = value;
+  }
+
+  get language(): string {
+    return this._language;
+  }
+
+  set language(value: string) {
+    this._language = value;
+  }
+}
