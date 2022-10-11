79078e0979604b8e7df779e2fa8aad451071d465
MarDem <m.demski@outlook.com>
2022-10-11 19:07:17 +0200

    Table presentation - final configuration.

--- src/app/shop/roof-windows/roof-windows.component.ts
+++ src/app/shop/roof-windows/roof-windows.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {RoofWindowSkylight} from '../../models/roof-window-skylight';
-import {filter, takeUntil} from 'rxjs/operators';
+import {filter, map, takeUntil} from 'rxjs/operators';
 import {Select, Store} from '@ngxs/store';
 import {RoofWindowState} from '../../store/roof-window/roof-window.state';
 import {Observable, Subject} from 'rxjs';
@@ -8,6 +8,7 @@ import _ from 'lodash';
 import {AddProductToCart} from '../../store/cart/cart.actions';
 import {CartState} from '../../store/cart/cart.state';
 import {Router} from '@angular/router';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-roof-windows',
@@ -37,8 +38,26 @@ export class RoofWindowsComponent implements OnInit, OnDestroy {
   cardPresentation = true;
   sortTableBy = 'popularityInTable';
   pageTableSize = 20;
+  filtrationForm: FormGroup;
+  nameToggler = true;
+  widthToggler = true;
+  heightToggler = true;
+  materialToggler = true;
+  glazingToggler = true;
+  ventilationToggler = true;
+  priceToggler = true;
+  filterObject = {
+    name: '',
+    width: '',
+    height: '',
+    material: '',
+    glazing: '',
+    ventilation: '',
+    price: ''
+  };
 
-  constructor(private store: Store, public router: Router) {
+  constructor(private store: Store, public router: Router,
+              private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
@@ -47,6 +66,26 @@ export class RoofWindowsComponent implements OnInit, OnDestroy {
     this.roofWindows$.pipe(takeUntil(this.isDestroyed$)).subscribe(roofWindows => this.roofWindowsList = roofWindows);
     this.filteredRoofWindowsList = this.roofWindowsList;
     this.cart$.pipe(filter(cart => cart.cart !== null), takeUntil(this.isDestroyed$)).subscribe(() => console.log);
+    this.filtrationForm = this.fb.group({
+      name: new FormControl(),
+      width: new FormControl(),
+      height: new FormControl(),
+      material: new FormControl(),
+      glazing: new FormControl(),
+      ventilation: new FormControl(),
+      price: new FormControl()
+    });
+    this.filtrationForm.valueChanges.pipe(
+      takeUntil(this.isDestroyed$),
+      map(data => {
+        this.filterObject.name = data.name;
+        this.filterObject.width = data.width;
+        this.filterObject.height = data.height;
+        this.filterObject.material = data.material;
+        this.filterObject.glazing = data.glazing;
+        this.filterObject.ventilation = data.ventilation;
+        this.filterObject.price = data.price;
+      })).subscribe(() => this.filterTable(this.filterObject));
     this.isFiltering = false;
     this.sortArray();
   }
@@ -174,6 +213,17 @@ export class RoofWindowsComponent implements OnInit, OnDestroy {
 
   changePresentation() {
     this.cardPresentation = !this.cardPresentation;
+    if (this.cardPresentation) {
+      this.filters.windowName = '';
+      this.filters.windowGlazings = [];
+      this.filters.windowMaterials = [];
+      this.filters.windowOpeningTypes = [];
+      this.filters.windowHeightFrom = 0;
+      this.filters.windowHeightTo = 999;
+      this.filters.windowWidthFrom = 0;
+      this.filters.windowWidthTo = 0;
+      this.filtersInput(this.filters);
+    }
   }
 
   sortTableArray() {
@@ -201,5 +251,84 @@ export class RoofWindowsComponent implements OnInit, OnDestroy {
 
   getTableRows() {
     return 20;
+  }
+
+  sortByName() {
+    this.nameToggler = !this.nameToggler;
+    const product = this.nameToggler ? 'asc' : 'desc';
+    this.filteredRoofWindowsList = _.orderBy(this.filteredRoofWindowsList, ['productName'], product);
+  }
+
+  sortByWidth() {
+    this.widthToggler = !this.widthToggler;
+    const product = this.widthToggler ? 'asc' : 'desc';
+    this.filteredRoofWindowsList = _.orderBy(this.filteredRoofWindowsList, ['szerokosc'], product);
+  }
+
+  sortByHeight() {
+    this.heightToggler = !this.heightToggler;
+    const product = this.heightToggler ? 'asc' : 'desc';
+    this.filteredRoofWindowsList = _.orderBy(this.filteredRoofWindowsList, ['wysokosc'], product);
+  }
+
+  sortByMaterial() {
+    this.materialToggler = !this.materialToggler;
+    const product = this.materialToggler ? 'asc' : 'desc';
+    this.filteredRoofWindowsList = _.orderBy(this.filteredRoofWindowsList, ['stolarkaMaterial'], product);
+  }
+
+  sortByGlazing() {
+    this.glazingToggler = !this.glazingToggler;
+    const product = this.glazingToggler ? 'asc' : 'desc';
+    this.filteredRoofWindowsList = _.orderBy(this.filteredRoofWindowsList, ['pakietSzybowy'], product);
+  }
+
+  sortByVentilation() {
+    this.ventilationToggler = !this.ventilationToggler;
+    const product = this.ventilationToggler ? 'asc' : 'desc';
+    this.filteredRoofWindowsList = _.orderBy(this.filteredRoofWindowsList, ['wentylacja'], product);
+  }
+
+  sortByPrice() {
+    this.priceToggler = !this.priceToggler;
+    const product = this.priceToggler ? 'asc' : 'desc';
+    this.filteredRoofWindowsList = _.orderBy(this.filteredRoofWindowsList, ['CenaDetaliczna'], product);
+  }
+
+  private filterTable(filterObject: { material: string; price: string; ventilation: string; name: string; width: string; glazing: string; height: string }) {
+    this.isFiltering = true;
+    this.filteredRoofWindowsList = this.roofWindowsList;
+    this.filteredRoofWindowsList = this.filteredRoofWindowsList.filter(window => {
+      let nameFound = true;
+      let widthFound = true;
+      let heightFound = true;
+      let materialFound = true;
+      let glazingFound = true;
+      let ventilationFound = true;
+      let priceFound = true;
+      if (filterObject.name) {
+        nameFound = window.productName.toString().trim().toLowerCase().indexOf(filterObject.name.toLowerCase()) !== -1;
+      }
+      if (filterObject.width) {
+        widthFound = window.szerokosc.toString().trim().toLowerCase().indexOf(filterObject.width.toLowerCase()) !== -1;
+      }
+      if (filterObject.height) {
+        heightFound = window.wysokosc.toString().trim().toLowerCase().indexOf(filterObject.height.toLowerCase()) !== -1;
+      }
+      if (filterObject.material) {
+        materialFound = window.stolarkaMaterial.toString().trim().toLowerCase().indexOf(filterObject.material.toLowerCase()) !== -1;
+      }
+      if (filterObject.glazing) {
+        glazingFound = window.pakietSzybowy.toString().trim().toLowerCase().indexOf(filterObject.glazing.toLowerCase()) !== -1;
+      }
+      if (filterObject.ventilation) {
+        ventilationFound = window.wentylacja.toString().trim().toLowerCase().indexOf(filterObject.ventilation.toLowerCase()) !== -1;
+      }
+      if (filterObject.price) {
+        priceFound = window.CenaDetaliczna.toString().trim().toLowerCase().indexOf(filterObject.price.toLowerCase()) !== -1;
+      }
+      return nameFound && widthFound && heightFound && materialFound && glazingFound && ventilationFound && priceFound;
+    });
+    this.isFiltering = false;
   }
 }
